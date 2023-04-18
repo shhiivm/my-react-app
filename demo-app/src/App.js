be54968bbddd5805bd1217631ce847a3fbe6562b
shhiivm <shhiivm@pm.me>
2023-04-18 14:34:50 +0530

    Components added

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,25 +1,5 @@
-import './App.css';
-
-function App() {
-  let name = 'react App'
-  return (
-    <div className="App">
-      <header className="App-header">
-          {name}
-          <Deo></Deo>
-      </header>
-    </div>
-  );
-}
-
-function Deo() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          Gap in react
-      </header>
-    </div>
-  );
+function App(){
+   return <div>Hemlo</div>
 }
 
 export default App;
