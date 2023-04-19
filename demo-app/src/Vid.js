ecdec04c13e7e44fa6cdc46ae14194cb99d42e69
shhiivm <shhiivm@pm.me>
2023-04-19 09:16:40 +0530

    image component added

---
+++ demo-app/src/Vid.js
@@ -0,0 +1,10 @@
+function Vid() {
+  return(
+    <div>
+        <img src="https://picsum.photos/id/1/200/300" alt="Katherine Johnson" />;
+        <div><h2>React components</h2></div>
+    </div>
+  )
+}
+
+export default Vid;
