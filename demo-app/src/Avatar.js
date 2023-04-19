94061cf0d40550b3efeeaaeb350ccec325f58b80
shhiivm <shhiivm@pm.me>
2023-04-19 13:17:02 +0530

    Props added to a React components

--- demo-app/src/Avatar.js
+++ demo-app/src/Avatar.js
@@ -1,19 +1,41 @@
-function Avatar(){
-    return(
-        <img
-            className="avatar"
-            src="https://i.imgur.com/1bX5QH6.jpg"
-            alt="Lin"
-            width={100}
-            height={100}
-        />
-    );
+import { getImageUrl } from './utils.js';
+
+function Avatar({ person, size }) {
+  return (
+    <img
+      className="avatar"
+      src={getImageUrl(person)}
+      alt={person.name}
+      width={size}
+      height={size}
+    />
+  );
 }
 
-export default function Profile(){
-    return(<>
-        <h1>Props in React</h1>
-        <Avatar/>
-        </>
-    );
+export default function Profile() {
+  return (
+    <div>
+      <Avatar
+        size={100}
+        person={{ 
+          name: 'Katsuko Saruhashi', 
+          imageId: 'YfeOqp2'
+        }}
+      />
+      <Avatar
+        size={80}
+        person={{
+          name: 'Aklilu Lemma', 
+          imageId: 'OKS67lh'
+        }}
+      />
+      <Avatar
+        size={50}
+        person={{ 
+          name: 'Lin Lanying',
+          imageId: '1bX5QH6'
+        }}
+      />
+    </div>
+  );
 }
