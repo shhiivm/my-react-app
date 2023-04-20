edce24c35a20b4da0b27bb019c5afaf849227f6a
shhiivm <shhiivm@pm.me>
2023-04-20 13:47:29 +0530

    Data of Yt Clone

---
+++ demo-app/src/YTClone/YtData.js
@@ -0,0 +1,82 @@
+const thumb = [
+    {
+    title:"Day 90 - Can't belive this is India. Is India Safe?",
+    channelName:"Angrez Ka Choda",
+    views:"10M",
+    time:"10 days",
+    id:'12',
+    logoId:"103",
+    verified:false
+
+},
+{       
+    title:"Day 50 -Travelling in Rural india. Is India Safe?",
+    channelName:"Angrez Ka Choda",
+    views:"26M",
+    time:"1 year",
+    id:'33',
+    logoId:"96",
+    verified:false
+},
+{
+    
+    title:"Riding Auto Rikshaw in India. How Safe India is to travel?.",
+    channelName:"Angrez Ka Choda",
+    views:"20M",
+    time:"2 weeks",
+    id:'45',
+    logoId:"69",
+    verified:false
+},
+{
+    title:"Ae Raja, raja! Music Video, Bhojpuri Superhit song- By Shyam Rangeela.",
+    channelName:"T-series",
+    views:"55M",
+    time:"8 yearss",
+    id:'145',
+    logoId:"169",
+    verified:true
+},
+
+
+
+{
+    title:"Learn C++ in 30 minutes. Beginner to advance",
+    channelName:"TeChutia",
+    views:"10M",
+    time:"3 years",
+    id:'20',
+    logoId:"30",
+    verified:false
+
+},
+{       
+    title:"How to Slap Someone online.",
+    channelName:"ShittyThings",
+    views:"26M",
+    time:"1 year",
+    id:'8',
+    logoId:"96",
+    verified:true
+},
+{
+    
+    title:"Dropping My Phone from space.",
+    channelName:"Techitt",
+    views:"550K",
+    time:"20 months",
+    id:'10',
+    logoId:"69",
+    verified:true
+},
+{
+    title:"Arijit Singh Remix songs.",
+    channelName:"T-series",
+    views:"305M",
+    time:"5 years",
+    id:'190',
+    logoId:"199",
+    verified:true
+}];
+
+export default thumb;
