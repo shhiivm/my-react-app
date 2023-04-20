9a2dd388e964e0cff12a0a69700ed7bc50f63835
shhiivm <shhiivm@pm.me>
2023-04-20 09:38:25 +0530

    stored all the video components in seperate file

---
+++ demo-app/src/data/data.js
@@ -0,0 +1,27 @@
+let videos =[
+    {
+      title: "React Js Tutorial",
+      views: "10.5k",
+      time: "10 days ago",
+      channel: "My Journo",
+      verified:true,
+      id:1
+    },
+    {
+      title: "Node Js Tutorial",
+      views: "2.2m",
+      time: "1 year ago",
+      channel: "My Journo",
+      verified:false,
+      id:3
+    },
+    {
+      title: "Mongo DB Tutorial",
+      views: "22.5k",
+      time: "1 month ago",
+      channel: "My Journo",
+      verified:true,
+      id:5
+    }
+  ];
+  export default videos;
