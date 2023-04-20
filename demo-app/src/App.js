07a955e212968425bcc5b22ab97b9e0467dd2eab
shhiivm <shhiivm@pm.me>
2023-04-20 09:40:05 +0530

    moved all the components to .data/data.js

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,29 +1,26 @@
 import Vid from "./Vid";
-import Avatar from "./Avatar"
-import Gallery  from "./Gallery";
-import './App.css';
+// import Avatar from "./Avatar"
+// import Gallery  from "./Gallery";
+import "./App.css";
+import videos from "./data/data";
 
-export default function App(){
-  return(
+export default function App() {
+
+  return (
     <>
-      <div>Hello</div>
+    <div>Videos</div>{
+      videos.map(video=>
       <Vid
-        title = "React Js Tutorial"
-        bgColor="Red"
-        colour = "Black"
-        views = "10.5K"
-        time= "10 Days ago"
-        channel= "My Journo"
-      />
-      <Vid
-        title = "Node Js Tutorial"
-        bgColor="Green"
-        colour = "white"
-        views = "2.2M"
-        time = "1 Year ago"
-        channel= "My Journo"
-      />
-
+        key={video.id}
+        title={video.title}
+        views={video.views}
+        time={video.time}
+        channel="My Journo"
+        verified={video.verified}
+        id={video.id}
+        
+      />)
+    }
 
       {/* <Gallery/>
       <Avatar/> */}
