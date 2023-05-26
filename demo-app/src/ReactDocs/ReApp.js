86f60271e118c499dac6fa49a06993f4a81497ee
shhiivm <shhiivm@pm.me>
2023-05-26 11:53:25 +0530

    initial commit

---
+++ demo-app/src/ReactDocs/ReApp.js
@@ -0,0 +1,9 @@
+import ReComp from './ReComp';
+export default function MyApp(){
+    return(
+        <div>
+        <h2>Welcome to MyApp</h2>
+        <ReComp/>
+        </div>
+    );
+}
---
+++ demo-app/src/ReactDocs/ReComp.js
@@ -0,0 +1,5 @@
+export default function MyButton(){
+    return(
+        <button>MyButton</button>
+    );
+}
